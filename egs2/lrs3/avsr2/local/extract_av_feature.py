c717413453985dfd709a7bdf37f4353ef1be770d
Julius Lehmann <lehmanju@devpi.de>
2024-11-21 16:13:06 +0100

    coding session
    Co-authored-by: val-ba <val-ba@users.noreply.github.com>
    Co-authored-by: MarisNH <MarisNH@users.noreply.github.com>
    Co-authored-by: WiebkeFr <WiebkeFr@users.noreply.github.com>

--- egs2/lrs3/avsr2/local/extract_av_feature.py
+++ egs2/lrs3/avsr2/local/extract_av_feature.py
@@ -15,13 +15,13 @@ import torch.nn.functional as F
 from face_alignment import VideoProcess
 from python_speech_features import logfbank
 from scipy.io import wavfile
+from torchvision.models import resnet18, ResNet18_Weights
+from einops import rearrange
+from torchvision.transforms import Resize
+from random import randint
+from torchvision.io import read_video
 
-from espnet2.asr.encoder.avhubert_encoder import FairseqAVHubertEncoder
 from espnet.utils.cli_writers import file_writer_helper
-
-url = "https://dl.fbaipublicfiles.com/avhubert/model/lrs3_vox/"
-base_url = url + "noise-pretrain/base_vox_iter5.pt"
-large_url = url + "noise-pretrain/large_vox_iter5.pt"
 
 
 def build_file_list(file_list):
@@ -33,18 +33,9 @@ def build_file_list(file_list):
 def load_video(data_filename):
     """load_video.
 
-    :param filename: str, the fileanme for a video sequence.
+    :param filename: str, the filename for a video sequence.
     """
-    frames = []
-    cap = cv2.VideoCapture(data_filename)
-    while cap.isOpened():
-        ret, frame = cap.read()  # BGR
-        if ret:
-            frames.append(frame)
-        else:
-            break
-    cap.release()
-    return np.array(frames)
+    return read_video(data_filename)[0] # only vid data, no fps info etc.
 
 
 def stacker(feats, stack_order):
@@ -70,71 +61,103 @@ def stacker(feats, stack_order):
     return feats
 
 
-def per_file(f, args, video_process, model, writer):
+def per_file(f, args, model, writer, train):
     id, f = f
     f_name = f"{os.sep}".join(os.path.normpath(f).split(os.sep)[-3:])[:-4]
     temp_aud_path = os.path.join(f"./data/temp/{args.gpu}", f_name)
     if not os.path.exists(os.path.dirname(temp_aud_path)):
         os.makedirs(os.path.dirname(temp_aud_path), exist_ok=True)
-    if os.path.exists(os.path.join(args.landmark_path, f_name + ".pkl")):
-        with open(os.path.join(args.landmark_path, f_name + ".pkl"), "rb") as pkl_file:
-            lm = pickle.load(pkl_file)
-        vid_name = f
-        vid = load_video(vid_name)
+    vid_name = f
+    vid = load_video(vid_name)
 
-        if all(x is None for x in lm) or len(vid) == 0:
-            return
+    # if len(vid) > 600:  ### whatever that was supposed to be useful for...
+    #     return
 
-        if len(vid) > 600:
-            return
+    #TODO do video extraction using roi crop + conv3d + resnet18
 
-        output = video_process(vid, lm)
-        if output is None:
-            return
+    ### cropping mouth region from video as mentioned in [1]
+    cropped_vid = vid[:, 72:-72, 47:-47, :].type(torch.float32)
 
-        os.system(
-            f"ffmpeg -loglevel panic -nostdin -y -i {vid_name} \
-                -acodec pcm_s16le -ar 16000 -ac 1 {temp_aud_path}.wav"
+    ### grayscaling
+    gray_vid = cropped_vid[..., 2] * 0.2125 + cropped_vid[..., 1] * 0.7154 + cropped_vid[..., 0] * 0.0721
+
+    ### resizing
+    gray_vid = rearrange(gray_vid, "N H W -> N 1 H W")  ### Resize wants it that way...
+    R = Resize((122, 122))
+    resized_vid = R(gray_vid[0])
+    for frame in range(gray_vid.shape[0] - 1):
+        resized_vid = torch.cat((resized_vid, R(gray_vid[frame + 1])))
+
+    if not train :
+        resized_vid = resized_vid[:, 5:-5, 5:-5]
+
+    # (random) choice of 112x112 crop from video as mentioned in [1]
+    else:
+        x, y = randint(0, 10), randint(0, 10)
+        if x == 10 and y == 10:
+            resized_vid = resized_vid[:, x:, y:]
+        elif x == 10:
+            resized_vid = resized_vid[:, x:, y : -(10 - y)]
+        elif y == 10:
+            resized_vid = resized_vid[:, x : -(10 - x), y:]
+        else:
+            resized_vid = resized_vid[:, x : -(10 - x), y : -(10 - y)]
+
+    resized_vid = (resized_vid - torch.mean(resized_vid)) / torch.std(resized_vid)
+
+
+    output = model.forward(vid)
+    #output = video_process(vid, lm)
+
+    if output is None:
+        return
+
+    os.system(
+        f"ffmpeg -loglevel panic -nostdin -y -i {vid_name} \
+            -acodec pcm_s16le -ar 16000 -ac 1 {temp_aud_path}.wav"
+    )
+
+    video_feats, _, _ = output  # T, H, W
+
+    sample_rate, wav_data = wavfile.read(f"{temp_aud_path}.wav")
+
+    #TODO audio preprocess
+    audio_feats = logfbank(wav_data, samplerate=sample_rate).astype(
+        np.float32
+    )  # [T, F]
+    # [T/stack_order_audio, F*stack_order_audio]
+    audio_feats = stacker(audio_feats, 4)
+    if audio_feats is not None and video_feats is not None:
+        diff = len(audio_feats) - len(video_feats)
+    if diff < 0:
+        audio_feats = np.concatenate(
+            [
+                audio_feats,
+                np.zeros([-diff, audio_feats.shape[-1]], dtype=audio_feats.dtype),
+            ]
         )
+    elif diff > 0:
+        audio_feats = audio_feats[:-diff]
+    audio_feats = torch.FloatTensor(audio_feats)
+    with torch.no_grad():
+        audio_feats = F.layer_norm(audio_feats, audio_feats.shape[1:])
+    audio_feats = audio_feats.unsqueeze(0)
 
-        video_feats, _, _ = output  # T, H, W
+    # [B, T, H, W, C] -> [B, C, T, H, W]
+    video_feats = (
+        torch.FloatTensor(video_feats).unsqueeze(-1).unsqueeze(0)
+    )  # B, T, H, W, C
+    video_feats = video_feats.permute(0, 4, 1, 2, 3).contiguous()
+    # [B, T, F] -> [B, F, T]
+    audio_feats = audio_feats.transpose(1, 2).contiguous()
 
-        sample_rate, wav_data = wavfile.read(f"{temp_aud_path}.wav")
-        audio_feats = logfbank(wav_data, samplerate=sample_rate).astype(
-            np.float32
-        )  # [T, F]
-        # [T/stack_order_audio, F*stack_order_audio]
-        audio_feats = stacker(audio_feats, 4)
-        if audio_feats is not None and video_feats is not None:
-            diff = len(audio_feats) - len(video_feats)
-        if diff < 0:
-            audio_feats = np.concatenate(
-                [
-                    audio_feats,
-                    np.zeros([-diff, audio_feats.shape[-1]], dtype=audio_feats.dtype),
-                ]
-            )
-        elif diff > 0:
-            audio_feats = audio_feats[:-diff]
-        audio_feats = torch.FloatTensor(audio_feats)
-        with torch.no_grad():
-            audio_feats = F.layer_norm(audio_feats, audio_feats.shape[1:])
-        audio_feats = audio_feats.unsqueeze(0)
+    # TODO feature concatenation
+    xs = {"video": video_feats.cuda(), "audio": audio_feats.cuda()}
+    feats = model.forward_fusion(xs)
 
-        # [B, T, H, W, C] -> [B, C, T, H, W]
-        video_feats = (
-            torch.FloatTensor(video_feats).unsqueeze(-1).unsqueeze(0)
-        )  # B, T, H, W, C
-        video_feats = video_feats.permute(0, 4, 1, 2, 3).contiguous()
-        # [B, T, F] -> [B, F, T]
-        audio_feats = audio_feats.transpose(1, 2).contiguous()
+    writer[id] = feats.squeeze(0).transpose(0, 1).contiguous().cpu().numpy()
 
-        xs = {"video": video_feats.cuda(), "audio": audio_feats.cuda()}
-        feats = model.forward_fusion(xs)
-
-        writer[id] = feats.squeeze(0).transpose(0, 1).contiguous().cpu().numpy()
-
-        os.system(f"rm {temp_aud_path}.wav")
+    os.system(f"rm {temp_aud_path}.wav")
     return
 
 
@@ -148,33 +171,14 @@ def main():
         write_num_frames=args.write_num_frames,
         compress=False,
     )
-
-    if args.model == "base":
-        model = FairseqAVHubertEncoder(
-            avhubert_url=base_url,
-            avhubert_dir_path=args.pretrained_model_dir,
-            encoder_embed_dim=768,
-            encoder_layers=12,
-            encoder_ffn_embed_dim=3072,
-            encoder_attention_heads=12,
-        )
-    else:
-        model = FairseqAVHubertEncoder(
-            avhubert_url=large_url,
-            avhubert_dir_path=args.pretrained_model_dir,
-            encoder_embed_dim=1024,
-            encoder_layers=24,
-            encoder_ffn_embed_dim=4096,
-            encoder_attention_heads=16,
-        )
+    train = args.train
+    #TODO new torch model for video feature extraction using roi crop + conv3d + resnet18
+    model = torch.nn.Sequential(torch.nn.Conv3d(1, 1, 5), resnet18(pretrained=True))
     model = model.cuda()
     model.eval()
-    video_process = VideoProcess(
-        mean_face_path="20words_mean_face.npy", convert_gray=True
-    )
     file_lists = build_file_list(args.file_list)
     for kk, f in enumerate(file_lists):
-        per_file(f, args, video_process, model, writer)
+        per_file(f, args, model, writer,train)
         if kk % 10 == 0:
             print(f"{kk}/{len(file_lists)}")
 
@@ -184,25 +188,11 @@ def get_parser():
         description="Command-line script for preprocessing."
     )
     parser.add_argument("--file_list", type=str, required=True, help="file_list (scp)")
-    parser.add_argument(
-        "--model", type=str, required=True, help="AV-HuBERT model config"
-    )
-    parser.add_argument(
-        "--pretrained_model_dir",
-        type=str,
-        default="./local/pre-trained",
-        help="AV-HuBERT pretrained model path",
-    )
     parser.add_argument("--gpu", type=int, required=True, help="GPU number (job - 1)")
-    parser.add_argument(
-        "--landmark_path",
-        type=str,
-        default="./local/LRS3_landmarks",
-        help="path including landmark files",
-    )
     parser.add_argument(
         "--write_num_frames", type=str, help="specify wspecifer for num frames"
     )
+    parser.add_argument("--train", action="store_true", help="turn on training mode")
     parser.add_argument("wspecifier", type=str, help="Write specifier")
     return parser
 
